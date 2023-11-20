33d9d0a6bbb17b09be154fec13e6d065fb8783fa
Dimentiy88 <bodartinov88@gmail.com>
2023-11-20 17:06:25 +0100

    end

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,57 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  private tenants: Person[] = [];
 
+  constructor(key: Key) {
+    this.key = key;
+  }
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door === true) {
+      this.tenants.push(person);
+      console.log("Двері відкріті.");
+    } else {
+      console.log("Двері закриті.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door === true;
+      console.log("Двері відкріті.");
+    } else {
+      console.log("Двері закриті.");
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
